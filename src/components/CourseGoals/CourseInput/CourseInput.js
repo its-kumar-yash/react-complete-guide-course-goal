03c9bf99e8547fd76d2f68d49b020cc8e84e194c
its-kumar-yash <yash-kumar.ug21@nsut.ac.in>
2023-02-11 19:38:15 +0530

    Styled Componebt & Dynamic Props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,45 @@
 import React, { useState } from "react";
-
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => props.notValid ? 'red' : 'black'};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => props.notValid ? 'red' : '#ccc'};
+    background: ${props => props.notValid ? '#ffd7d7' : 'transparent'}
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  /* Dynamic Styling */
+
+  ${'' /* &.invalid input {
+    border-color: red;
+    background: rgb(240, 190, 190);
+  }
+
+  &.invalid label {
+    color: red;
+  } */}
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -28,7 +66,7 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* dynamic styling */}
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl notValid = {!isValid}>
         <label>Course Goal</label>
         <input
           // Conditional Styling
@@ -39,7 +77,7 @@ const CourseInput = (props) => {
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
